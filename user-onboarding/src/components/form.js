88b1ba7b68e199e7d7678b9ed1dda5b872f3545f
duellal <alexandriaduell@gmail.com>
2022-07-29 19:02:05 -0400

    Added styling to form + made a theme color for page - need to style user divs next

--- user-onboarding/src/components/form.js
+++ user-onboarding/src/components/form.js
@@ -16,111 +16,132 @@ export default function Form(props) {
 
    return (
       <form className='form container' onSubmit={onSubmit}>
-         <div className='form input'>
-            <h2>Add User</h2>
-            <label>
-               First Name
-               <span className='first-name input-box'>
-                  <input
-                     type='text'
-                     value={values.first_name}
-                     onChange={onChange}
-                     name='first_name'
-                  />
-               </span>
-               <div className='error'>{errors.first_name}</div>
-            </label>
+         <h2>Add User</h2>
+         <div className='name'>
+            <div className='first-name'>
+               <label>
+                  First Name
+                  <div className='input-box'>
+                     <input
+                        type='text'
+                        value={values.first_name}
+                        onChange={onChange}
+                        name='first_name'
+                     />
+                  </div>
+                  <div className='error'>{errors.first_name}</div>
+               </label>
+            </div>
 
-            <label>
-               Last Name
-               <span className='last-name input-box'>
-                  <input
-                     type='text'
-                     value={values.last_name}
-                     onChange={onChange}
-                     name='last_name'
-                  />
-               </span>
-               <div className='error'>{errors.last_name}</div>
-            </label>
-
-            <label>
-               Email
-               <span className='email input-box'>
-                  <input
-                     type='text'
-                     name='email'
-                     value={values.email}
-                     onChange={onChange}
-                  />
-               </span>
-               <div className='error'>{errors.email}</div>
-            </label>
-
-            <label>
-               Role
-               <span className='role input-box'>
-                  <select
-                     name='role'
-                     value={values.role}
-                     onChange={onChange}
-                  >
-                     <option value=''>Select Role</option>
-                     <option value='Back End Developer'>Back End Developer</option>
-                     <option value='Data Scientist'>Data scientist</option>
-                     <option value='Front End Developer'>Front End Developer</option>
-                     <option value='Team Lead'>Team Lead</option>
-                     <option value='UX Designer'>UX Designer</option>
-                  </select>
-               </span>
-               <div className='error'>{errors.role}</div>
-            </label>
-
-            <label>
-               Password
-               <span className='password input-box'>
-                  <input
-                     type='text'
-                     name='password'
-                     value={values.password}
-                     onChange={onChange}
-                  />
-               </span>
-               <div className='error'>{errors.password}</div>
-            </label>
-
-            <label>
-               Confirm Password
-               <span className='password input-box'>
-                  <input
-                     type='text'
-                     name='confirmPassword'
-                     value={values.confirmPassword}
-                     onChange={onChange}
-                  />
-               </span>
-               <div className='error'>{errors.confirmPassword}</div>
-            </label>
-
-            <label>
-               Terms of Service
-               <div className='terms'>
-                  <p>
-                     Lorem Ipsum...
-                  </p>
-               </div>
-               <span className='formTerms input-box'>
-                  <p>I agree to the terms of service</p>
-                  <input
-                     type='checkbox'
-                     name='terms'
-                     checked={values.terms}
-                     onChange={onChange}
-                  />
-               </span>
-               <div className='error'>{errors.terms}</div>
-            </label>
+            <div className='last-name'>
+               <label>
+                  Last Name
+                  <div className='input-box'>
+                     <input
+                        type='text'
+                        value={values.last_name}
+                        onChange={onChange}
+                        name='last_name'
+                     />
+                  </div>
+                  <div className='error'>{errors.last_name}</div>
+               </label>
+            </div>
          </div>
+
+         <div className='email-role'>
+            <div className='email'>
+               <label>
+                  Email
+                  <div className='input-box'>
+                     <input
+                        type='text'
+                        name='email'
+                        value={values.email}
+                        onChange={onChange}
+                     />
+                  </div>
+                  <div className='error'>{errors.email}</div>
+               </label>
+            </div>
+
+            <div className='role'>
+               <label>
+                  Role
+                  <div className='input-box'>
+                     <select
+                        name='role'
+                        value={values.role}
+                        onChange={onChange}
+                     >
+                        <option value=''>Select Role</option>
+                        <option value='Back End Developer'>Back End Developer</option>
+                        <option value='Data Scientist'>Data Scientist</option>
+                        <option value='Front End Developer'>Front End Developer</option>
+                        <option value='Team Lead'>Team Lead</option>
+                        <option value='UX Designer'>UX Designer</option>
+                     </select>
+                  </div>
+                  <div className='error'>{errors.role}</div>
+               </label>
+            </div>
+         </div>
+
+         <div className='password-confirm'>
+            <div className='password'>
+               <label>
+                  Password
+                  <div className='input-box'>
+                     <input
+                        type='text'
+                        name='password'
+                        value={values.password}
+                        onChange={onChange}
+                     />
+                  </div>
+                  <div className='error'>{errors.password}</div>
+               </label>
+            </div>
+
+            <div className='password'>
+               <label>
+                  Confirm Password
+                  <div className='input-box'>
+                     <input
+                        type='text'
+                        name='confirmPassword'
+                        value={values.confirmPassword}
+                        onChange={onChange}
+                     />
+                  </div>
+                  <div className='error'>{errors.confirmPassword}</div>
+               </label>
+            </div>
+         </div>
+
+         <div className='formTerms'>
+            <h3>Terms of Service</h3>
+            <div className='terms'>
+               <p>
+                  Lorem Ipsum...
+               </p>
+            </div>
+            <div className='checkbox input-box'>
+               <div className='box'>
+                  <label>
+                     <input
+                        type='checkbox'
+                        name='terms'
+                        checked={values.terms}
+                        onChange={onChange}
+                     />
+                  </label>
+               </div>
+               <p>I agree to the terms of service</p>
+            </div>
+            <div className='error'>{errors.terms}</div>
+         </div>
+
 
          <div className='submit-button'>
             <button className='submit' disabled={disabled}>Submit</button>
